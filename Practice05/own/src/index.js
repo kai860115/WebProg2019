6a8ab32839ecf6498184fb14ab3a5b3ed7b7a008
kai860115 <b04501127@ntu.edu.tw>
2019-04-07 18:57:54 +0800

    remove Practice05/own

--- Practice05/own/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './style.css';
-import './reset.css';
-import CalcApp from './containers/CalcApp';
-
-// eslint
-ReactDOM.render(<CalcApp />, document.getElementById('root'));
